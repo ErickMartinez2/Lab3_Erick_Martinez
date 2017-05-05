232bc961597042f5058a90e4e398f4e1adf35885
Erick Martinez <Erick Martinez>
2017-05-05 15:24:22 -0600

    Modificacion Jugadores completada

--- Lab3_Erick_Martinez/src/lab3_erick_martinez/Lab3_Erick_Martinez.java
+++ Lab3_Erick_Martinez/src/lab3_erick_martinez/Lab3_Erick_Martinez.java
@@ -43,12 +43,17 @@ public class Lab3_Erick_Martinez {
                     Crear();
                     break;
                 case 2:
+                    Modificar();
+                    break;
+                case 3:
+                    
                     break;
             }
         } while (opcion != 10);
     }
 
     public static void Crear() {
+        System.out.println();
         System.out.println("-> Crear Jugador");
         System.out.print("Ingrese el nombre: ");
         String nombre = sc.next();
@@ -56,18 +61,11 @@ public class Lab3_Erick_Martinez {
         String apellido = sc.next();
         System.out.print("Ingrese la edad: ");
         int edad = sc.nextInt();
-        System.out.println(" Estado:");
-        System.out.println("1. Libre");
-        System.out.println("2. Comprado");
-        System.out.print("Ingrese el numero del estado: ");
-        int estado = sc.nextInt();
-        sc.nextLine();
         System.out.print("Ingrese el pais de nacimiento: ");
+        sc.nextLine();
         String pais = sc.nextLine();
         System.out.print("Ingrese el pie preferido: ");
         String pie = sc.next();
-        System.out.print("Ingrese el numero de camiseta: ");
-        int camiseta = sc.nextInt();
         System.out.print("Ingrese el precio: ");
         double precio = sc.nextDouble();
         System.out.println(" Posicion");
@@ -87,7 +85,7 @@ public class Lab3_Erick_Martinez {
                 double velocidad = sc.nextDouble();
                 System.out.print("Ingrese su promedio de goles por partido: ");
                 double promedio = sc.nextDouble();
-                Jugadores J_D = new Delantero(definicion, altura, velocidad, promedio, nombre, apellido, edad, estado, pais, pie, camiseta, precio, "");
+                Jugadores J_D = new Delantero(definicion, altura, velocidad, promedio, nombre, apellido, edad, 1, pais, pie, 0, precio, "");
                 jugador.add(J_D);
                 break;
             case 2:
@@ -98,7 +96,7 @@ public class Lab3_Erick_Martinez {
                 int dominio = sc.nextInt();
                 System.out.print("Ingrese su promedio de asistencias por partido: ");
                 double promedio2 = sc.nextDouble();
-                Jugadores J_M = new Medio(creatividad, dominio, promedio2, nombre, apellido, edad, estado, pais, pie, camiseta, precio, "");
+                Jugadores J_M = new Medio(creatividad, dominio, promedio2, nombre, apellido, edad, 1, pais, pie, 0, precio, "");
                 jugador.add(J_M);
                 break;
             case 3:
@@ -111,7 +109,7 @@ public class Lab3_Erick_Martinez {
                 double peso = sc.nextDouble();
                 System.out.print("Ingrese su velocidad: ");
                 double velocidad2 = sc.nextDouble();
-                Jugadores J_D2 = new Defensa(agresividad, altura2, peso, velocidad2, nombre, apellido, edad, estado, pais, pie, camiseta, precio, "");
+                Jugadores J_D2 = new Defensa(agresividad, altura2, peso, velocidad2, nombre, apellido, edad, 1, pais, pie, 0, precio, "");
                 jugador.add(J_D2);
                 break;
             case 4:
@@ -120,8 +118,179 @@ public class Lab3_Erick_Martinez {
                 int aereo = sc.nextInt();
                 System.out.print("Ingrese su nivel con los pies (1-10): ");
                 int pies = sc.nextInt();
-                Jugadores J_P = new Portero(aereo, pies, nombre, apellido, edad, estado, pais, pie, camiseta, precio, "");
+                Jugadores J_P = new Portero(aereo, pies, nombre, apellido, edad, 1, pais, pie, 0, precio, "");
                 jugador.add(J_P);
+                break;
+        }
+    }
+
+    public static void Modificar() {
+        System.out.println();
+        System.out.println("-> Modificar Jugador");
+        System.out.println(" Jugadores:");
+        for (int i = 0; i < jugador.size(); i++) {
+            System.out.println(i + ". " + jugador.get(i));
+        }
+        System.out.print("Ingrese el numero del jugador: ");
+        int num = sc.nextInt();
+        System.out.println(" Informacion General");
+        System.out.println("1. Nombre");
+        System.out.println("2. Apellido");
+        System.out.println("3. Edad");
+        System.out.println("4. Pais de Nacimiento");
+        System.out.println("5. Pie Preferido");
+        System.out.println("6. Precio");
+        System.out.println("7. Informacion Detallada");
+        System.out.print("Ingrese su opcion: ");
+        int opcion2 = sc.nextInt();
+        System.out.println(" Modificacion");
+        switch (opcion2) {
+            case 1:
+                System.out.print("Ingrese el nombre: ");
+                String nombre = sc.next();
+                jugador.get(num).setNombre(nombre);
+                break;
+            case 2:
+                System.out.print("Ingrese el apellido: ");
+                String apellido = sc.next();
+                jugador.get(num).setApellido(apellido);
+                break;
+            case 3:
+                System.out.print("Ingrese la edad: ");
+                int edad = sc.nextInt();
+                jugador.get(num).setEdad(edad);
+                break;
+            case 4:
+                System.out.print("Ingrese el pais de nacimiento: ");
+                sc.nextLine();
+                String pais = sc.nextLine();
+                jugador.get(num).setPais(pais);
+                break;
+            case 5:
+                System.out.print("Ingrese el pie preferido: ");
+                String pie = sc.next();
+                jugador.get(num).setPie(pie);
+                break;
+            case 6:
+                System.out.print("Ingrese el precio: ");
+                double precio = sc.nextDouble();
+                jugador.get(num).setPrecio(precio);
+                break;
+            case 7:
+                System.out.println();
+                if (jugador.get(num) instanceof Delantero) {
+                    System.out.println(" Delantero:");
+                    System.out.println("1. Nivel de Definicion");
+                    System.out.println("2. Altura");
+                    System.out.println("3. Velocidad");
+                    System.out.println("4. Promedio de Goles");
+                    System.out.print("Ingrese su opcion: ");
+                    int opcion3 = sc.nextInt();
+                    System.out.println();
+                    switch (opcion3) {
+                        case 1:
+                            System.out.print("Ingrese el nivel de definicion: ");
+                            int definicion = sc.nextInt();
+                            ((Delantero) jugador.get(num)).setNivel_definicion(definicion);
+                            break;
+                        case 2:
+                            System.out.print("Ingrese la altura: ");
+                            double altura = sc.nextDouble();
+                            ((Delantero) jugador.get(num)).setAltura(altura);
+                            break;
+                        case 3:
+                            System.out.print("Ingrese la velocidad: ");
+                            double velocidad = sc.nextDouble();
+                            ((Delantero) jugador.get(num)).setVelocidad(velocidad);
+                            break;
+                        case 4:
+                            System.out.print("Ingrese el promedio de goles por partido: ");
+                            double promedio = sc.nextDouble();
+                            ((Delantero) jugador.get(num)).setPromedio(promedio);
+                            break;
+                    }
+                } else {
+                    if (jugador.get(num) instanceof Medio) {
+                        System.out.println(" Medio:");
+                        System.out.println("1. Nivel de Creatividad");
+                        System.out.println("2. Nivel de Dominio");
+                        System.out.println("3. Promedio de Asistencias");
+                        System.out.print("Ingrese su opcion: ");
+                        int opcion3 = sc.nextInt();
+                        System.out.println();
+                        switch (opcion3) {
+                            case 1:
+                                System.out.print("Ingrese el nivel de creatividad: ");
+                                int creatividad = sc.nextInt();
+                                ((Medio) jugador.get(num)).setNivel_creatividad(creatividad);
+                                break;
+                            case 2:
+                                System.out.print("Ingrese el nivel de dominio: ");
+                                int dominio = sc.nextInt();
+                                ((Medio) jugador.get(num)).setNivel_dominio(dominio);
+                                break;
+                            case 3:
+                                System.out.print("Ingrese el promedio de asistencias: ");
+                                double promedio = sc.nextDouble();
+                                ((Medio) jugador.get(num)).setPromedio(promedio);
+                                break;
+                        }
+                    } else {
+                        if (jugador.get(num) instanceof Defensa) {
+                            System.out.println(" Defensa:");
+                            System.out.println("1. Nivel de Agresividad");
+                            System.out.println("2. Altura");
+                            System.out.println("3. Peso");
+                            System.out.println("4. Velocidad");
+                            System.out.print("Ingrese su opcion: ");
+                            int opcion3 = sc.nextInt();
+                            System.out.println();
+                            switch (opcion3) {
+                                case 1:
+                                    System.out.print("Ingrese el nivel de agresividad: ");
+                                    int agresividad = sc.nextInt();
+                                    ((Defensa) jugador.get(num)).setNivel_agresividad(agresividad);
+                                    break;
+                                case 2:
+                                    System.out.print("Ingrese la altura: ");
+                                    double altura = sc.nextDouble();
+                                    ((Defensa) jugador.get(num)).setAltura(altura);
+                                    break;
+                                case 3:
+                                    System.out.print("Ingrese el peso: ");
+                                    double peso = sc.nextDouble();
+                                    ((Defensa) jugador.get(num)).setPeso(peso);
+                                    break;
+                                case 4:
+                                    System.out.print("Ingrese la velocidad: ");
+                                    double velocidad = sc.nextDouble();
+                                    ((Defensa) jugador.get(num)).setVelocidad(velocidad);
+                                    break;
+                            }
+                        } else {
+                            if (jugador.get(num) instanceof Portero) {
+                                System.out.println(" Portero:");
+                                System.out.println("1. Nivel de Juego Aereo");
+                                System.out.println("2. Nivel de Juego con los Pies");
+                                System.out.print("Ingrese su opcion: ");
+                                int opcion3 = sc.nextInt();
+                                System.out.println();
+                                switch (opcion3) {
+                                    case 1:
+                                        System.out.print("Ingrese el nivel de juego aereo: ");
+                                        int aereo = sc.nextInt();
+                                        ((Portero) jugador.get(num)).setNivel_aereo(aereo);
+                                        break;
+                                    case 2:
+                                        System.out.print("Ingrese el nivel de juego con los pies: ");
+                                        int pies = sc.nextInt();
+                                        ((Portero) jugador.get(num)).setNivel_pies(pies);
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
                 break;
         }
     }
